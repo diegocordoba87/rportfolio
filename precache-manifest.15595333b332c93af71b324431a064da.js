d26f55531388b8b211eab50e90205e445d8f866a
Diego Cordoba <diegocordoba87@hotmail.com>
2020-08-06 11:10:55 -0400

    Updates

---
+++ precache-manifest.15595333b332c93af71b324431a064da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef83975d81095e29d50df07c17a0437e",
+    "url": "/rportfolio/index.html"
+  },
+  {
+    "revision": "9c9fd3c8800a87d426c5",
+    "url": "/rportfolio/static/js/2.034db02a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rportfolio/static/js/2.034db02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8eb1d1bd1f45cf21e7a",
+    "url": "/rportfolio/static/js/main.747df04b.chunk.js"
+  },
+  {
+    "revision": "c016b552e95f16b844cc",
+    "url": "/rportfolio/static/js/runtime-main.edee0638.js"
+  },
+  {
+    "revision": "6f410f4163b1f22e6cda1c096835e97d",
+    "url": "/rportfolio/static/media/DiegoCordoba.6f410f41.png"
+  },
+  {
+    "revision": "f5154bc21026c22c3f95a79acf36976f",
+    "url": "/rportfolio/static/media/EntertainMeMain.f5154bc2.png"
+  },
+  {
+    "revision": "98e0d50460c1611322c149e797788a98",
+    "url": "/rportfolio/static/media/WorkDaySchedulerMain-1.98e0d504.png"
+  },
+  {
+    "revision": "18a7c2c6d94fd6b6cc694ef1704153d9",
+    "url": "/rportfolio/static/media/background.18a7c2c6.jpg"
+  },
+  {
+    "revision": "5e0d662d7ccef9fdf65a27b546028025",
+    "url": "/rportfolio/static/media/password_generator_main.5e0d662d.png"
+  },
+  {
+    "revision": "3130debbaa803a29af007b1dca0bb17e",
+    "url": "/rportfolio/static/media/workHardPlayHard.3130debb.png"
+  }
+]);
