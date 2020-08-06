a4d09a1107074750005e34fa879e8497bc0a4ed8
Diego Cordoba <diegocordoba87@hotmail.com>
2020-08-06 17:30:26 -0400

    added server.js script

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const express =require("express");
-const mongoose =  require("mongoose");
-
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 const app = express();
-
 const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"))
-}
-
-app.use(routes);
-
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/rportfolio").then(()=>{
-    console.log("Succesfully connected to database.")
-}).catch((err)=>{
-    console.log("Uanble to connect to database.")
-})
-
-app.listen(PORT,function (){
-    console.log(`Espress server is running on http://localhost:${PORT}`)
-})
+app.use(express.static("client/build"));
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/my-portfolio", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to database.");
+  })
+  .catch((err) => {
+    console.log("Unable to connect to database.");
+    console.log(err);
+  });
+app.listen(PORT, () => {
+  console.log(`Express server running on http://localhost:${PORT}`);
+});
